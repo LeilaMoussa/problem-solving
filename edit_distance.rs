9c845b32f90dbc501bbcf6e5fe02ec5b8d31064c
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2021-04-10 23:20:52 +0100

    dp

--- edit_distance.rs
+++ edit_distance.rs
@@ -1,17 +1,20 @@
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(unused)]
+
 // Minimum cost to covert string1 to string2
 
 use std::cmp;
 
 fn main() {
-	let string1: &str = "hello";
-	let string2: &str = "henlow";
+	const string1: &str = "hello";
+	const string2: &str = "henlo!";
 
-	let moves_recursive: usize = edit_distance_recursive(string1.get(..), string2.get(..));
-	println!("Total moves {}", moves_recursive);
+	// let moves_recursive: usize = edit_distance_recursive(string1.get(..), string2.get(..));
+	// println!("Total moves {}", moves_recursive);
 
-	// let moves_dp: usize = edit_distance_dp(r1, r2);
-
-	// assert_eq!(moves_recursive, moves_dp);
+	let moves_dp = edit_distance_dp(string1, string2, string1.len(), string2.len());
+	println!("Total moves {:?}", moves_dp);
 
 	// let ascii_moves: usize = edit_distance_ascii(r1, r2);
 
@@ -19,12 +22,36 @@ fn main() {
 
 }
 
+fn edit_distance_dp(string1: &str, string2: &str, n: usize, m: usize) -> usize {
+	let mut dp = vec![vec![0; m+1]; n+1];
+
+	for i in 0..n+1 {
+		for j in 0..m+1 {
+			if i == 0 {
+				dp[i][j] = j;
+				// ED(n, m) == m when n == 0
+			}
+			else if j == 0 {
+				dp[i][j] = i;
+			}
+			else if string1.as_bytes()[i-1] == string2.as_bytes()[j-1] {
+				// same last char, trim off the last
+				dp[i][j] = dp[i-1][j-1];
+			}
+			else {
+				dp[i][j] = 1 + cmp::min(dp[i-1][j], cmp::min(dp[i][j-1], dp[i-1][j-1]));
+			}
+		}
+	}
+	dp[n][m]
+}
+
 fn edit_distance_recursive(s1: Option<&str>, s2: Option<&str>) -> usize {
     let string1: &str;
     let string2: &str;
 
-	match s1 {
-		Some(x) => string1 = x, // reference rather?
+	match s1 {					// not digging this
+		Some(x) => string1 = x,
 		None => {
 			println!("Problemo.");
 			return 0;
@@ -47,20 +74,14 @@ fn edit_distance_recursive(s1: Option<&str>, s2: Option<&str>) -> usize {
 		return n;
 	}
 
-	if string1.as_bytes()[n-1] == string2.as_bytes()[m-1] {  // same byte representation, i don't care about the actual char
-		// aligned so far ==> look before the last characters now
+	if string1.as_bytes()[n-1] == string2.as_bytes()[m-1] { 
 		return edit_distance_recursive(string1.get(0..n-1), string2.get(0..m-1));
 	}
-	// otherwise, there either needs to be a substitution, deletion, or insertion
 	return 1 + cmp::min(edit_distance_recursive(string1.get(0..n-1), string2.get(0..m)),
 				cmp::min(edit_distance_recursive(string1.get(0..n), string2.get(0..m-1)),
 							edit_distance_recursive(string1.get(0..n-1), string2.get(0..m-1))));
-	// cmp::min takes 2 vals
+	// cmp::min takes 2 vals only lol
 }
-
-// fn edit_distance_dp() {
-
-// }
 
 // fn edit_distance_ascii() {
 
